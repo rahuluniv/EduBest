fddb531f887fdcbee46179895e906641afbdeb5f
rahuluniv <rahulunivoffical@gmail.com>
2024-03-15 03:20:52 -0600

    Basic Django Setup

--- backend/rcat_project/settings.py
+++ backend/rcat_project/settings.py
@@ -36,14 +36,38 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS  = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
 ]
+SITE_ID = 1
+
+LOCAL_APPS = [
+    "common",
+    "accounts",
+    "profiles",
+    "schools",
+    "assessments",
+    "enquiries",
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "django_filters",
+    "django_countries",
+    "phonenumber_field",
+    "djoser",
+    "rest_framework_simplejwt",
+    "djcelery_email",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
